7622c86bff5b2334e1a3eef7ba156fc1fefe89f3
Alfredo Fernandez <alfredo@hotmail.com>
2021-06-21 07:22:20 -0400

    add midleware

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '007482c69035df8bb16df6965943f771'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '93525829-3ed3-48f3-bed3-49ef78daffad'
+};
